e34709de8d8e2e00892e8da65c4f9b09c0e62165
jeevan <jeevanmuchakarla107@gmail.com>
2026-02-23 09:19:44 +0530

    Initial Commit

---
+++ ChildCare AITests/ChildCare_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  ChildCare_AITests.swift
+//  ChildCare AITests
+//
+//  Created by G1 on 23/02/26.
+//
+
+import Testing
+@testable import ChildCare_AI
+
+struct ChildCare_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
